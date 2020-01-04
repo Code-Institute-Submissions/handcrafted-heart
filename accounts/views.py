1308d941a8021e7aa5246575121996d659afba35
Ubuntu <ubuntu@ip-172-31-33-38.ec2.internal>
2020-01-04 15:48:29 +0000

    Add the registration functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib import messages, auth
 from django.urls import reverse
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm
 from django.template.context_processors import csrf
 from django.contrib.auth.decorators import login_required
 
@@ -9,18 +9,18 @@ from django.contrib.auth.decorators import login_required
 # The views.
 
 def index(request):
-    """A view that display the index page"""
+    """A view to display the index page"""
     return render(request, "index.html")
     
 def logout(request):
-    """A view that logs the user out and redirects back to the index page"""
+    """A view to log the user out and redirect back to the index page"""
     auth.logout(request)
     messages.success(request, 'You have successfully logged out')
     return redirect(reverse('index'))
 
 
 def login(request):
-    """A view that manages the login form"""
+    """A view to manage the login form"""
     if request.method == 'POST':
         user_form = UserLoginForm(request.POST)
         if user_form.is_valid():
@@ -47,5 +47,29 @@ def login(request):
 
 @login_required
 def profile(request):
-    """A view that displays the profile page of a logged in user"""
+    """A view to display the profile page of a logged in user"""
     return render(request, 'profile.html')
+    
+    
+def register(request):
+    """A view to manage the registration form"""
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+
+            user = auth.authenticate(request.POST.get('email'),
+                                     password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('index'))
+
+            else:
+                messages.error(request, "Unable to log you in at the moment!")
+    else:
+        user_form = UserRegistrationForm()
+
+    args = {'user_form': user_form}
+    return render(request, 'register.html', args)
