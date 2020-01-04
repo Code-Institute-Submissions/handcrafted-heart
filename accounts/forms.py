d0aca79a0a654c731066c6aff08dbe17041c62b4
Ubuntu <ubuntu@ip-172-31-33-38.ec2.internal>
2020-01-04 15:17:30 +0000

    Create login and registration forms for users

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,1 +1,28 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+# The forms
+
+class UserLoginForm(forms.Form):
+    """Form to be used to log users in"""
+    
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    
+    
+class UserRegistrationForm(UserCreationForm):
+    """Form used to register a new user"""
+
+    password1 = forms.CharField(
+        label="Password", 
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password Confirmation",
+        widget=forms.PasswordInput)
+      
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
+        
