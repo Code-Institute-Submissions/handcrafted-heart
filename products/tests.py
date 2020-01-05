11f6d9957ad2c752fa4100df9599003d2ece7a34
Ubuntu <ubuntu@ip-172-31-33-38.ec2.internal>
2020-01-05 15:09:56 +0000

    Add first test of the models

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Product
 
 # Create your tests here.
+class ProductTests(TestCase):
+    
+      """
+      Define tests here that will be
+      run against the Product models.
+      """
+      
+      # Test the name of the product.
+      def test_str(self):
+        test_name = Product(name='A product')
+        self.assertEqual(str(test_name), 'A product')
