6e78d26ca398ec783d8b9ea1b9322f453afd5e07
Ubuntu <ubuntu@ip-172-31-33-38.ec2.internal>
2020-01-11 15:40:50 +0000

    Insert an if statement to use the database url if db-url are in env variables

--- handcraftedheart/settings.py
+++ handcraftedheart/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+#import env
 import dj_database_url
+
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,15 +86,20 @@ WSGI_APPLICATION = 'handcraftedheart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-    
+  
+# if the database URL is in environment variables then use the database URL, which is the cloud database,
+# otherwise, use the sqlite database. 
+ 
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }  
 
 
 # Password validation
