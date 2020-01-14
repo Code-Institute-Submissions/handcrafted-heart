f5e6b2e069561702885079aa07d2fa4e6bac84b0
Ubuntu <ubuntu@ip-172-31-33-38.ec2.internal>
2020-01-14 12:57:10 +0000

    Change url to index page, redirect all_products url

--- handcraftedheart/urls.py
+++ handcraftedheart/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from accounts.views import index
 from accounts import urls as urls_accounts
 from products import urls as urls_products
 from cart import urls as urls_cart
@@ -26,9 +27,10 @@ from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', all_products, name='index'),
+    url(r'^$', index, name="index"),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
+    url(r'^products/', all_products, name='all_products'),
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
