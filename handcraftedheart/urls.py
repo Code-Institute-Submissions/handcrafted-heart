f41d9a888ffb6a80ce973d85bc85d5ddb2599a30
Ubuntu <ubuntu@ip-172-31-33-38.ec2.internal>
2020-01-15 16:15:46 +0000

    Try and add home app in url patterns to be able to show contact page

--- handcraftedheart/urls.py
+++ handcraftedheart/urls.py
@@ -20,6 +20,7 @@ from accounts import urls as urls_accounts
 from products import urls as urls_products
 from cart import urls as urls_cart
 from search import urls as urls_search
+from home import urls as urls_home
 from checkout import urls as urls_checkout
 from products.views import all_products
 from django.views import static
@@ -34,5 +35,6 @@ urlpatterns = [
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
+    url(r'^home/', include(urls_home)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
