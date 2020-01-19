58c92ea8514c3cf81c7c92ac7d2dee9fd1b091e6
Ubuntu <ubuntu@ip-172-31-33-38.ec2.internal>
2020-01-19 16:15:09 +0000

    Change to add for more clear naming of the purpose

--- inspiration/urls.py
+++ inspiration/urls.py
@@ -4,6 +4,6 @@ from .views import get_inspiration, inspiration_content, create_or_edit_inspirat
 urlpatterns = [
     url(r'^$', get_inspiration, name='get_inspiration'),
     url(r'^(?P<pk>\d+)/$', inspiration_content, name='inspiration_content'),
-    url(r'^added/$', create_or_edit_inspiration, name='added_inspiration'),
+    url(r'^add/$', create_or_edit_inspiration, name='add_inspiration'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_inspiration, name='edit_inspiration'),
 ]
