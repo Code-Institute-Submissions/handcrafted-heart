11f33e4de533de901dca7a90eb315050d45d517d
Ubuntu <ubuntu@ip-172-31-33-38.ec2.internal>
2020-01-19 14:29:23 +0000

    Add the views for the inspiration pages

--- inspiration/views.py
+++ inspiration/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Inspiration
+from .forms import InspirationSharingForm
 
 # Create your views here.
+
+def get_inspiration(request):
+        inspiration = Inspiration.objects.all()
+        return render(request, "inspiration.html", {'inspiration': inspiration})
+    
+        
+
+def inspiration_content(request, pk):
+    if request.method == "GET":
+        inspiration = get_object_or_404(Inspiration, pk=pk)
+        inspiration.save()
+        return render(request, "inspiration.html", {'inspiration': inspiration})
+    if request.method == "POST":
+        inspiration = get_object_or_404(Inspiration, pk=pk)
+        inspiration.save()
+        return redirect('/inspiration/', {'inspiration': inspiration})
+
+
+
+def create_or_edit_inspiration(request, pk=None):
+    inspiration = get_object_or_404(Inspiration, pk=pk) if pk else None
+    if request.method == "POST":
+        form = InspirationSharingForm(request.POST, request.FILES, instance=inspiration)
+        if form.is_valid():
+            inspiration = form.save()
+            return redirect('/inspiration/', {'inspiration': inspiration})
+    else:
+        form = InspirationSharingForm(instance=inspiration)
+    return render(request, 'inspirationform.html', {'form': form})
