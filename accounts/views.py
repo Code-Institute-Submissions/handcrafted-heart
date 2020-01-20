8e993613079c905c653c48e9d1c5149e9d37c826
Ubuntu <ubuntu@ip-172-31-33-38.ec2.internal>
2020-01-20 11:05:04 +0000

    Change the imports

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.shortcuts import render, redirect, reverse, HttpResponseRedirect
 from django.contrib import messages, auth
-from django.urls import reverse
-from .forms import UserLoginForm, UserRegistrationForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
 from django.template.context_processors import csrf
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
 
 # The views.
 
